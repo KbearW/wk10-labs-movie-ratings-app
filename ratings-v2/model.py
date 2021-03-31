b12d0b7a65c8cba5b8d40ef8df0c20a4b788abe2
Kerrie <wongkerrie@gmail.com>
2021-03-31 03:22:00 +0000

    completed task #1- update the class

--- ratings-v2/model.py
+++ ratings-v2/model.py
@@ -5,7 +5,20 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-# Replace this with your code!
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = __users__
+
+    user_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True
+                        )
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(10), nullable=False)
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}>'
 
 
 def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
