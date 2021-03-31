aa6536912a4400115558ef6eb29862085d06076a
Kerrie <wongkerrie@gmail.com>
2021-03-31 03:23:18 +0000

    completed task #1- update the class

--- ratings-v2/model.py
+++ ratings-v2/model.py
@@ -8,14 +8,14 @@ db = SQLAlchemy()
 class User(db.Model):
     """A user."""
 
-    __tablename__ = __users__
+    __tablename__ = 'users'
 
     user_id = db.Column(db.Integer,
                         autoincrement=True,
                         primary_key=True
                         )
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(10), nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
 
     def __repr__(self):
         return f'<User user_id={self.user_id} email={self.email}>'
